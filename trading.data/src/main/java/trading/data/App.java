5b3ab44661f926c0c44e3d33f274c9037c8ca5de
dmitry <dmitry@ubuntu1>
2014-05-26 16:48:01 +0400

    Changed to Java 8

--- trading.data/src/main/java/trading/data/App.java
+++ trading.data/src/main/java/trading/data/App.java
@@ -2,26 +2,24 @@ package trading.data;
 
 //import javax.persistence.EntityManager;
 
-import trading.data.model.Instrument;
-
 import org.hibernate.Session;
+
+import trading.data.model.Instrument;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-   
-    	Session session = HibernateUtil.getSessionFactory().openSession();
-    	
-    	Instrument i = (Instrument)session.get(Instrument.class, 1);
-    	
-    	//EntityManager em = JPAUtil.getEntitymanager();
-    	//Instrument i = em.find(Instrument.class, 1);
-    	String name = (i!=null)?i.getName():"";
-        System.out.println( "Instrument:" + name );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		Instrument i = (Instrument) session.get(Instrument.class, 1);
+
+		// EntityManager em = JPAUtil.getEntitymanager();
+		// Instrument i = em.find(Instrument.class, 1);
+		String name = (i != null) ? i.getName() : "";
+		System.out.println("Instrument:" + name);
+	}
 }
